3bed6d6ead7b790ebf942f869fbd5da2b68db848
Kristjan <kristjan.pet@gmail.com>
2026-03-28 12:06:33 +0100

    Added toglle up/down test

--- Window-blinds-Controller/test/test_motor_controller.cpp
+++ Window-blinds-Controller/test/test_motor_controller.cpp
@@ -10,10 +10,23 @@ void test_motor_moving_up(void){
     BlindsController blinds(fMotor);
 
     TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(MoveCommand::UP));
+    TEST_ASSERT_EQUAL(BlindsState::MOVING_UP, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::UP, fMotor.getLastAction());
+}
+
+void test_toggle_style_up_down(void){
+    FakeMotor fMotor;
+    BlindsController blinds(fMotor);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(MoveCommand::UP));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(MoveCommand::DOWN));
+    TEST_ASSERT_EQUAL(BlindsState::IDLE, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::STOP, fMotor.getLastAction());
 }
 
 extern "C" void app_main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_motor_moving_up);
+    RUN_TEST(test_toggle_style_up_down);
     UNITY_END();
 }
