530c05220558c9b0234e72b34cf676bfd7e618af
Kristjan <kristjan.pet@gmail.com>
2026-05-14 09:41:15 +0200

    Add tests for stall recovery scenarios in BlindsController

--- Window-blinds-Controller/test/fakes/FakeMotor.hpp
+++ Window-blinds-Controller/test/fakes/FakeMotor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <climits>
 #include <cstdint>
 
 #include "IMotor.hpp"
@@ -17,14 +18,19 @@ private:
     esp_err_t nextMoveResult = ESP_OK;
     esp_err_t nextStopResult = ESP_OK;
     int32_t currentStep = 100;
+    int32_t maxStep = INT_MAX;
     int32_t lastTargetStep = 0;
     bool lastMoveWasCalibrating = false;
+    uint32_t moveCalls = 0;
     uint32_t moveToMaxCalls = 0;
+    uint32_t stopCalls = 0;
 public:
     LastAction getLastAction(){return lastAction;};
     int32_t getLastTargetStep() const {return lastTargetStep;}
     bool wasLastMoveCalibrating() const {return lastMoveWasCalibrating;}
+    uint32_t getMoveCalls() const {return moveCalls;}
     uint32_t getMoveToMaxCalls() const {return moveToMaxCalls;}
+    uint32_t getStopCalls() const {return stopCalls;}
     void setNextResult(esp_err_t result){
         nextMoveResult = result;
         nextStopResult = result;
@@ -32,18 +38,23 @@ public:
     void setNextMoveResult(esp_err_t result){nextMoveResult = result;}
     void setNextStopResult(esp_err_t result){nextStopResult = result;}
     void setCurrentStep(int32_t step){currentStep = step;}
+    void setMaxStepValue(int32_t step){maxStep = step;}
 
     void reset(){
         lastAction = LastAction::NONE;
         nextMoveResult = ESP_OK;
         nextStopResult = ESP_OK;
         currentStep = 100;
+        maxStep = INT_MAX;
         lastTargetStep = 0;
         lastMoveWasCalibrating = false;
+        moveCalls = 0;
         moveToMaxCalls = 0;
+        stopCalls = 0;
     }
 
     esp_err_t move(int32_t targetStep, bool isCalibrating = false) override {
+        moveCalls++;
         lastTargetStep = targetStep;
         lastMoveWasCalibrating = isCalibrating;
         if(!isCalibrating && targetStep >= currentStep){
@@ -57,16 +68,20 @@ public:
 
     esp_err_t moveToMax() override {
         moveToMaxCalls++;
+        lastTargetStep = maxStep;
+        lastMoveWasCalibrating = false;
         lastAction = LastAction::UP;
         return nextMoveResult;
     }
 
     esp_err_t stop() override {
+        stopCalls++;
         lastAction = LastAction::STOP;
         return nextStopResult;
     }
 
     void setHoming(int32_t offset = 0) override {currentStep = 0 - offset;}
-    void setMaxStep(int32_t offset = 0) override {(void)offset;}
+    void setMaxStep(int32_t offset = 0) override {maxStep = currentStep - offset;}
     int32_t getCurrentStep() const override {return currentStep;}
+    int32_t getMaxStep() const override {return maxStep;}
 };
--- Window-blinds-Controller/test/test_motor_controller.cpp
+++ Window-blinds-Controller/test/test_motor_controller.cpp
@@ -10,6 +10,15 @@
 void setUp(void) {}
 void tearDown(void) {}
 
+static void complete_up_stall_recovery(BlindsController& blinds, FakeMotor& fMotor, int32_t stallStep){
+    fMotor.setCurrentStep(stallStep);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::STALL_RECOVERY, blinds.getState());
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
+    TEST_ASSERT_EQUAL(BlindsState::MOVING_UP, blinds.getState());
+}
+
 void test_motor_moving_up(void){
     FakeMotor fMotor;
     BlindsCommandQueue queue;
@@ -122,6 +131,176 @@ void test_limit_reached_stop_failure_enters_fault(void){
     TEST_ASSERT_EQUAL(ESP_ERR_INVALID_ARG, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
     TEST_ASSERT_EQUAL(BlindsState::FAULT, blinds.getState());
     TEST_ASSERT_EQUAL(LastAction::STOP, fMotor.getLastAction());
+}
+
+void test_up_stall_backs_off_and_enters_recovery(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::STALL_RECOVERY, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::DOWN, fMotor.getLastAction());
+    TEST_ASSERT_EQUAL(10000 - AppConfig::normalStallBackoffSteps, fMotor.getLastTargetStep());
+    TEST_ASSERT_EQUAL_UINT32(1, fMotor.getStopCalls());
+}
+
+void test_down_stall_backs_off_clamped_to_max_and_enters_recovery(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setMaxStepValue(5000);
+    fMotor.setCurrentStep(4000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::DOWN));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::STALL_RECOVERY, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::UP, fMotor.getLastAction());
+    TEST_ASSERT_EQUAL(5000, fMotor.getLastTargetStep());
+}
+
+void test_stall_recovery_limit_reached_retries_original_up_target(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
+    TEST_ASSERT_EQUAL(BlindsState::MOVING_UP, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::UP, fMotor.getLastAction());
+    TEST_ASSERT_EQUAL_UINT32(2, fMotor.getMoveToMaxCalls());
+    TEST_ASSERT_EQUAL_UINT32(2, fMotor.getStopCalls());
+}
+
+void test_stall_recovery_limit_reached_retries_original_down_target(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setMaxStepValue(20000);
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::DOWN));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
+    TEST_ASSERT_EQUAL(BlindsState::MOVING_DOWN, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::DOWN, fMotor.getLastAction());
+    TEST_ASSERT_EQUAL(0, fMotor.getLastTargetStep());
+    TEST_ASSERT_EQUAL_UINT32(2, fMotor.getStopCalls());
+}
+
+void test_stall_recovery_retry_stop_failure_enters_fault(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    fMotor.setNextStopResult(ESP_FAIL);
+
+    TEST_ASSERT_EQUAL(ESP_FAIL, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
+    TEST_ASSERT_EQUAL(BlindsState::FAULT, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::STOP, fMotor.getLastAction());
+    TEST_ASSERT_EQUAL_UINT32(1, fMotor.getMoveToMaxCalls());
+}
+
+void test_three_stall_recoveries_are_allowed_then_fourth_stall_faults(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+
+    complete_up_stall_recovery(blinds, fMotor, 10000);
+    complete_up_stall_recovery(blinds, fMotor, 11000);
+    complete_up_stall_recovery(blinds, fMotor, 12000);
+
+    fMotor.setCurrentStep(13000);
+
+    TEST_ASSERT_EQUAL(ESP_ERR_INVALID_STATE, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::FAULT, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::STOP, fMotor.getLastAction());
+}
+
+void test_target_reached_resets_stall_recovery_count(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+
+    complete_up_stall_recovery(blinds, fMotor, 10000);
+    complete_up_stall_recovery(blinds, fMotor, 11000);
+    complete_up_stall_recovery(blinds, fMotor, 12000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::LIMIT_REACHED));
+    TEST_ASSERT_EQUAL(BlindsState::IDLE, blinds.getState());
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    fMotor.setCurrentStep(13000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::STALL_RECOVERY, blinds.getState());
+}
+
+void test_stop_resets_stall_recovery_count(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+
+    complete_up_stall_recovery(blinds, fMotor, 10000);
+    complete_up_stall_recovery(blinds, fMotor, 11000);
+    complete_up_stall_recovery(blinds, fMotor, 12000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STOP));
+    TEST_ASSERT_EQUAL(BlindsState::IDLE, blinds.getState());
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    fMotor.setCurrentStep(13000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::STALL_RECOVERY, blinds.getState());
+}
+
+void test_stall_stop_failure_enters_fault(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    fMotor.setNextStopResult(ESP_FAIL);
+
+    TEST_ASSERT_EQUAL(ESP_FAIL, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::FAULT, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::STOP, fMotor.getLastAction());
+}
+
+void test_stall_recovery_move_failure_enters_fault(void){
+    FakeMotor fMotor;
+    BlindsCommandQueue queue;
+    BlindsController blinds(fMotor, queue);
+
+    fMotor.setCurrentStep(10000);
+
+    TEST_ASSERT_EQUAL(ESP_OK, blinds.handleCommand(BlindsEvent::UP));
+    fMotor.setNextMoveResult(ESP_FAIL);
+
+    TEST_ASSERT_EQUAL(ESP_FAIL, blinds.handleCommand(BlindsEvent::STALL_DETECTED));
+    TEST_ASSERT_EQUAL(BlindsState::FAULT, blinds.getState());
+    TEST_ASSERT_EQUAL(LastAction::DOWN, fMotor.getLastAction());
 }
 
 void test_move_up_failure_from_idle(void){
@@ -301,6 +480,16 @@ extern "C" void app_main(void) {
     RUN_TEST(test_stop_failure_while_moving_up);
     RUN_TEST(test_limit_reached_stops_and_sets_idle);
     RUN_TEST(test_limit_reached_stop_failure_enters_fault);
+    RUN_TEST(test_up_stall_backs_off_and_enters_recovery);
+    RUN_TEST(test_down_stall_backs_off_clamped_to_max_and_enters_recovery);
+    RUN_TEST(test_stall_recovery_limit_reached_retries_original_up_target);
+    RUN_TEST(test_stall_recovery_limit_reached_retries_original_down_target);
+    RUN_TEST(test_stall_recovery_retry_stop_failure_enters_fault);
+    RUN_TEST(test_three_stall_recoveries_are_allowed_then_fourth_stall_faults);
+    RUN_TEST(test_target_reached_resets_stall_recovery_count);
+    RUN_TEST(test_stop_resets_stall_recovery_count);
+    RUN_TEST(test_stall_stop_failure_enters_fault);
+    RUN_TEST(test_stall_recovery_move_failure_enters_fault);
     RUN_TEST(test_move_up_failure_from_idle);
     RUN_TEST(test_move_down_failure_from_idle);
     RUN_TEST(test_calibrate_move_failure_enters_fault);
